993fc2b4ba7f8055252a117439569ec015b0a92e
supan0228 <2470919708@qq.com>
2021-04-20 20:29:51 +0800

    页面画完
    第二次提交

--- src/views/course/audio.vue
+++ src/views/course/audio.vue
@@ -1,24 +1,51 @@
 <template>
-  <el-form :inline="true" :model="formInline" class="demo-form-inline">
-    <el-form-item>
-      <el-button type="primary">新增音频</el-button>
-    </el-form-item>
-    <el-form-item>
-      <el-select v-model="formInline.region" placeholder="商品状态">
-        <el-option label="已下架" value="已下架"></el-option>
-        <el-option label="已上架" value="已上架"></el-option>
-      </el-select>
-    </el-form-item>
-    <el-form-item>
-      <el-input v-model="formInline.user" placeholder="标题"></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="onSubmit">搜索</el-button>
-    </el-form-item>
+<div>
+    <el-row class="main" :gutter="20">
+      <el-col :span="6">
+        <el-button class="button" type="primary">
+          <i class="el-icon-edit"></i>新增音频</el-button
+        >
+      </el-col>
+      <el-col :span="12">
+        <el-select v-model="value" placeholder="商品状态" class="select">
+          <el-option style="color:#606266">已上架 </el-option>
+          <el-option style="color:#606266"> 已下架 </el-option>
+        </el-select>
+      </el-col>
+      <el-col :span="6" class="search">
+        <el-input
+          v-model="input"
+          placeholder="请输入内容"
+          class="input"
+        ></el-input>
+        <el-button class="searchB" type="primary">
+          <i class="el-icon-search"></i>搜索</el-button
+        >
+      </el-col>
+    </el-row>
+
     <el-table :data="tableData" style="width: 100%" border>
       <el-table-column sortable prop="id" label="ID" width="100" align="center">
       </el-table-column>
       <el-table-column prop="content" label="音频内容" width="500">
+          <template slot-scope="{ row }">
+          <div style="display: flex">
+            <img
+              :src="row.cover"
+              style="width: 100px; height: 50px; margin-right: 10px"
+            />
+            <div
+              style="
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+              "
+            >
+              <span>{{ row.title }}</span>
+              <span style="color: red">￥{{ row.price }}</span>
+            </div>
+          </div>
+        </template>
       </el-table-column>
       <el-table-column
         prop="sub_count"
@@ -76,7 +103,8 @@
       >
       </el-pagination>
     </div>
-  </el-form>
+</div>
+  
 </template>
   
 
@@ -92,32 +120,7 @@ export default {
       currentPage4: 4,
       currentPage5: 5,
 
-      tableData: [
-        {
-          id: "1",
-          content: "王小虎bvhdbdjvbjzdnbkjnbknkvnknk",
-          sub_count: "12",
-          status: "普陀区",
-          created_time: "2021-4-20-13:40",
-          try: 200333,
-        },
-        {
-          id: "2",
-          content: "王小虎bvhdbdjvbjzdnbkjnbknkvnknk",
-          sub_count: "13",
-          status: "普陀区",
-          created_time: "2021-4-20-14:40",
-          try: 200333,
-        },
-        {
-          id: "3",
-          content: "王小虎bvhdbdjvbjzdnbkjnbknkvnknk",
-          sub_count: "14",
-          status: "普陀区",
-          created_time: "2021-4-20-15:40",
-          try: 200333,
-        },
-      ],
+      tableData: [],
       formInline: {
         user: "",
         region: "",
@@ -125,18 +128,47 @@ export default {
     };
   },
   methods: {
-    onSubmit() {
-      console.log("submit!");
-    },
     handleSizeChange(val) {
       console.log(`每页 ${val} 条`);
     },
     handleCurrentChange(val) {
       console.log(`当前页: ${val}`);
     },
+    async getlist() {
+      let res = await fetchList();
+      console.log(res);
+      if (res.code = 20000) {
+        this.tableData = res.data.items;
+      }
+    },
+  },
+  created() {
+    this.getlist();
   },
 };
 </script>
 <style scoped>
+.main {
+  margin: 20px;
+}
+.button {
+  margin: 5px 0 0 5px;
+}
+.searchB {
+  margin-right: 0;
+}
+.input {
+  width: 200px;
+  height: 36px;
+}
+.select {
+  width: 110px;
+  height: 36px;
+  float: right;
+}
+
+.search {
+  float: right;
+}
 </style>
 
